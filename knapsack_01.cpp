7d950fa0e2260ff91119c611f16d4b3b9c280f73
Carolyn <carolyngychan@gmail.com>
2020-11-10 18:36:42 -0800

    Knapsack unique weights

--- knapsack_01.cpp
+++ knapsack_01.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <set>
 #include <vector>
 
 /*
- * Tabulization method - Calculate max profit for each capacity from 
+ * Tabulation method - Calculate max profit for each capacity from 
  * 0 -> capacity. At each step, check all weights. For all weights under
  * current capacity, calculate its profit by taking that weight's profit
  * and also the max profit previously calculated for capacity - weight.
@@ -10,23 +11,44 @@
 int knapsack(std::vector<int> weights, std::vector<int> profits, int capacity) {
     // Keep track of max profit at each capacity from 0 -> capacity 
     std::vector<int> dp(capacity + 1);
+    std::vector<std::set<int>> dpWeights(capacity + 1);
 
     // Calculate max profit for capacity from 0 -> capacity
     for (unsigned c = 0; c <= capacity; c++) {
          int maxProfit = 0;
+         std::set<int> maxProfitWeights;
 
          // Take all weights that are less than capacity. 
          // Calculate max profit including that weight and check if it 
          // maximizes total max profit for current capacity
          for (unsigned j = 0; j < weights.size(); j++) {
             if (weights[j] <= c) {
-                int currProfit = profits[j] + dp[c - weights[j]];
-                maxProfit = std::max(maxProfit, currProfit);
+                int currWeight = weights[j];
+                
+                // If weight was not used earlier
+                if (dpWeights[c - currWeight].find(currWeight) == dpWeights[c - currWeight].end()) {
+                    
+                    // Calculate profit 
+                    int currProfit = profits[j] + dp[c - currWeight];
+                    
+                    // Add current weight and weights from dp into set
+                    std::set<int> currWeights;
+                    currWeights.insert(currWeight);
+                    currWeights.insert(dpWeights[c - currWeight].begin(),
+                        dpWeights[c - currWeight].end());
+                    
+                    // If profit is maximal, update max vars
+                    if (currProfit > maxProfit) {
+                        maxProfit = currProfit;
+                        maxProfitWeights = currWeights;
+                    }
+                }
             }
          }
         
          // Store calculated max profit for current capacity
          dp[c] = maxProfit;
+         dpWeights[c] = maxProfitWeights;
     }
 
     // Return calculated max profit for requested capacity
